9f37fd3cf8b40a46ad6ef6d984a7429d565c9178
natanieljr <nataniel.borges@cispa.saarland>
2020-04-09 20:56:49 +0200

    Updated config + script to run on colossus

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,78 +1,114 @@
-echo "Usage: ./launch.sh <APK_DIR> <OUT_DIR>"
+echo "Usage: ./launch.sh <APK_DIR> <OUT_DIR> [<NUM_RUNS> (default 1]"
 
+DEFAULT_SLEEP=5
 APK_DIR=$1
 OUT_DIR=$2
+NUM_RUNS=$3 || 1
 APP=$(ls -a $APK_DIR/*.apk | sort | head -1)
 DM=../app/build/outputs/apk/release/app-release.apk
 
 ./uninstall_apk.sh "$APP" || true
 
-sleep 1
-
 ./uninstall_apk.sh $DM || true
 
-sleep 1
-
-adb install "$APP"
-
-sleep 1
-
-adb install $DM
-
-sleep 1
+sleep $DEFAULT_SLEEP
 
 # Push config
-Adb push defaultConfig.properties /sdcard/
+adb push defaultConfig.properties /sdcard/
 
-# Force some window updates to prevent errors when launching the app
-adb shell input keyevent 84
+echo "Creating output dir $OUT_DIR"
+mkdir $OUT_DIR
 
-sleep 1
+#!/bin/bash
+for ((i=0; i<NUM_RUNS; i++))
+do
+    echo "Removing old DM-2 files on device"
+    adb shell rm -rf /sdcard/DM-2
 
-adb shell input keyevent 3
+    adb install "$APP"
+    adb install $DM
 
-sleep 1
+    # Force some window updates to prevent errors when launching the app
+    echo "Launching search"
+    adb shell input keyevent 84
+    sleep $DEFAULT_SLEEP
 
-# Launch app
-adb shell am start -n "org.droidmate.accessibility/org.droidmate.accessibility.MainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER -D
+    echo "Pressing home"
+    adb shell input keyevent 3
+    sleep $DEFAULT_SLEEP
 
-sleep 1
+    # Launch app
+    echo "Launching app"
+    adb shell am start -n "org.droidmate.accessibility/org.droidmate.accessibility.MainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER -D
+    sleep $DEFAULT_SLEEP
 
-# Click runtime permission "allow" [691,1017][876,1143]
-adb shell input tap 700 1050
+    echo "Click runtime permission \"allow\" [691,1017][876,1143]"
+    adb shell input tap 700 1050
+    sleep $DEFAULT_SLEEP
 
-sleep 1
+    echo "Click \"start now\" for screen recording [711,1005][978,1131]"
+    adb shell input tap 750 1100
+    sleep $DEFAULT_SLEEP
 
-# Click "start now" for screen recording [711,1005][978,1131]
-adb shell input tap 750 1100
+    # Click on app
+    echo "Click on app"
+    adb shell input tap 500 300
+    sleep $DEFAULT_SLEEP
 
-sleep 1
+    echo "Click on accessibility in the menu"
+    adb shell input tap 500 600
+    sleep $DEFAULT_SLEEP
 
-# Click on app
-adb shell input tap 500 300
+    echo "Click on toggle to activate app"
+    adb shell input tap 1000 300
+    sleep $DEFAULT_SLEEP
 
-sleep 1
+    echo "Click on ok to start accessibility [810,1258][978,1384]"
+    adb shell input tap 850 1300
 
-# Click on accessibility in the menu
-adb shell input tap 500 600
+    # Wait exploration to complete
+    TOTAL_TIME=0
+    while : ; do
+        sleep 30
+        # Break if the "exploration done" signal file is there
+        RESULT=$(adb shell "[ -f /sdcard/DM-2/exploration.done ] || echo 1")
+        [[ $RESULT == 1 ]] || break
+        # Break after 3h
+        if ((TOTAL_TIME > 10800)); then break; fi
+        echo "Waiting DM-2 to finish";
+    done
 
-sleep 1
+    echo "Click on toggle to deactivate app"
+    adb shell input tap 1000 300
+    sleep $DEFAULT_SLEEP
 
-# Click on toggle to activate app
-adb shell input tap 1000 300
+    echo "Click on ok to stop accessibility [810,998][978,1124]"
+    adb shell input tap 850 1000
+    sleep $DEFAULT_SLEEP
 
-sleep 1
+    echo "Press back"
+    adb shell input keyevent 4
+    sleep $DEFAULT_SLEEP
 
-# Click on ok to start accessibility [810,1258][978,1384]
-adb shell input tap 850 1300
+    echo "Press back"
+    adb shell input keyevent 4
+    sleep $DEFAULT_SLEEP
 
-# Wait exploration to complete
-while : ; do
-    sleep 30
-    RESULT=$(adb shell "[ -f /sdcard/DM-2/exploration.done ] || echo 1")
-    [[ $RESULT == 1 ]] || break
-    echo "File does not exist";
+    echo "Press back"
+    adb shell input keyevent 4
+    sleep $DEFAULT_SLEEP
+
+    echo "Press back"
+    adb shell input keyevent 4
+    sleep $DEFAULT_SLEEP
+
+    ./uninstall_apk.sh $DM || true
+
+    ./uninstall_apk.sh "$APP" || true
+
+    echo "Pulling results"
+    adb pull /sdcard/DM-2/ $OUT_DIR/$i
+
 done
 
-adb pull /sdcard/DM-2/ $OUT_DIR/
-
+echo "Done"
